1b919019dac9d1da42b1b90c8edb9fd301f265b7
rishabhenu <rishabhenu@gmail.com>
2019-08-06 22:07:34 +0530

    added web project

---
+++ src/com/externalpricingservices/services/requests/RmsPricingRequest.java
@@ -0,0 +1,14 @@
+package com.externalpricingservices.services.requests;
+
+public class RmsPricingRequest {
+	
+	private int id = 10;
+
+	@Override
+	public String toString() {
+		return "RmsPricingRequest [id=" + id + "]";
+	}
+	
+	
+
+}
