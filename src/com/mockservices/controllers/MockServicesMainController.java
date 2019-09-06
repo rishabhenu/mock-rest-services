f9ff848ea894fef7f3d6397bc62d94e2abbed8c7
rishabhenu <rishabhenu@gmail.com>
2019-09-06 23:10:13 +0530

    Redirected to https in case of http

--- src/com/mockservices/controllers/MockServicesMainController.java
+++ src/com/mockservices/controllers/MockServicesMainController.java
@@ -3,6 +3,7 @@ package com.mockservices.controllers;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +15,9 @@ public class MockServicesMainController {
 		String sb = (request.getRequestURL()).substring(0, (request.getRequestURL().length()-request.getRequestURI().length()));
 		
 		String context = request.getContextPath();
+		if(!isLocalHost(sb) && sb.contains("http://")) {
+			sb = sb.replace("http://", "https://");
+		}
 		request.setAttribute("baseUrl", sb.toString()+context);
 		request.setAttribute("context", context);
 		return "index";
@@ -23,5 +27,12 @@ public class MockServicesMainController {
 		String regex = "(.*)([a-zA-Z])(:)([0-9]+)";
 		boolean b = url.matches(regex);
 		return b;
+	}
+	
+	public static void main(String...strings) {
+		String url = "http://localhost:8082";
+		String regex = "(.*)([a-zA-Z])(:)([0-9]+)";
+		boolean b = url.matches(regex);
+		System.out.println(b);
 	}
 }
