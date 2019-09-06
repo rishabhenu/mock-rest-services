d674d68f080ec9f7c7a139f42b3f5c2ba9a4f764
rishabhenu <rishabhenu@gmail.com>
2019-09-06 23:36:17 +0530

    Removed package import

--- src/com/mockservices/controllers/MockServicesMainController.java
+++ src/com/mockservices/controllers/MockServicesMainController.java
@@ -3,7 +3,6 @@ package com.mockservices.controllers;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.junit.Test;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
